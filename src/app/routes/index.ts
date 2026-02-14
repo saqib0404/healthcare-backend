f7af7225a36bbfd1b3be8cc5a1cd2c8fdf3a0514
Saqib <saqibahmad0404@gmail.com>
2026-02-15 00:20:00 +0600

    auth basic setted

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { SpecialtyRouter } from "../modules/specialty/specialty.router";
+import { AuthRouter } from "../modules/auth/auth.router";
 
 const router = Router();
 
+router.use("/auth", AuthRouter)
 router.use("/specialties", SpecialtyRouter);
 
 export const IndexRouter = router;
