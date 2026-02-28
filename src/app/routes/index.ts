18aafcc3c26aa320146459e39e4b2254ac144fcc
saqib0404 <saqibahmad0404@gmail.com>
2026-02-28 20:12:43 +0600

    user model added

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { SpecialtyRouter } from "../modules/specialty/specialty.router";
 import { AuthRouter } from "../modules/auth/auth.router";
+import { userRoutes } from "../modules/user/user.route";
 
 const router = Router();
 
 router.use("/auth", AuthRouter)
 router.use("/specialties", SpecialtyRouter);
+router.use("/users", userRoutes);
 
 export const IndexRouter = router;
